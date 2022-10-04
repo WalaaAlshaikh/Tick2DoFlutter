8f3c2015c762de813428bd1543887d18a6ae2831
walaaalshaikh <walaalshaikh94@gmail.com>
2022-10-04 10:06:12 +0300

    styling the bottom navigation bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tick2do/main.dart';
 import '';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -12,6 +11,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final tabs = [
+      Container(),
+      Container(),
+    ];
+    int selectedIndex = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(MyApp.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white.withOpacity(0.5),
+        selectedItemColor: Colors.white,
+        currentIndex: selectedIndex,
+        onTap: (index) => setState(() {
+          selectedIndex = index;
+        }),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fact_check_outlined),
+            label: 'Todos',
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done, size: 28),
+            label: 'Compeleted',
+          )
+        ],
+      ),
+    );
   }
 }
